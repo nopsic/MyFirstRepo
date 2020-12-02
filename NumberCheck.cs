7b11e8e3ba386685a4a093621fc01894abdf2873
Havasdi <daniel.havasdi.ext@evosoft.com>
2020-12-02 11:29:52 +0100

    Update NumberCheck.cs

--- NumberCheck.cs
+++ NumberCheck.cs
@@ -1,1 +1,23 @@
+public class NumberCheck
+{
 
+    void OddOrEven(int number)
+    {
+        if(number % 2 == 1)
+        {
+            System.Console.WriteLine($"Your number {number} is an odd number.");
+        }
+        else
+        {
+            System.Console.WriteLine($"Your number {number} is an even number.");
+        }
+    }
+
+    static void Main(string args)
+    {
+        System.Console.WriteLine("Please give me your number!");
+        int numberToCheck = Console.Readline();
+        OddOrEven(numberToCheck);
+
+    }
+}
